e6cbae116cdb2d12100a29f0f88f7e785744623f
Ignacio Cano <i.cano@gbh.com.do>
2020-01-18 20:24:25 -0400

    pending readme

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath=icano/udacity-prediction
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+# docker login
+docker tag $dockerpath:latest $dockerpath:${1:-latest}
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
